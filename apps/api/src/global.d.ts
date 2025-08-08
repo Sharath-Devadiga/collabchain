f2c196fc952f9f73a530947b1b067e785a119095
Parth2684 <bhosle6006@gmail.com>
2025-08-08 23:50:16 +0530

    extended the types and fixed them and added updated env

--- apps/api/src/global.d.ts
+++ apps/api/src/global.d.ts
@@ -1,28 +1,12 @@
 // global.d.ts
 import "elysia";
+import type { Context as ElysiaContext } from "elysia/context";
 
 declare module "elysia" {
-  interface Context {
-    query: Record<string, string | undefined>
-
+  interface Context extends ElysiaContext {
     jwt: {
       sign: (payload: Record<string, any>) => Promise<string> | string;
       verify: (token?: string) => Promise<any> | any;
-    };
-
-    cookie: {
-      [key: string]: {
-        get: () => string | undefined;
-        set: (options: {
-          value: string;
-          httpOnly?: boolean;
-          path?: string;
-          sameSite?: 'strict' | 'lax' | 'none';
-          secure?: boolean;
-          maxAge?: number;
-        }) => void;
-        remove: () => void;
-      };
     };
   }
 }
